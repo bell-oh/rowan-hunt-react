0c985f127edc569892979e17b64f44e1b516ac34
oliver bello <obello26@icloud.com>
2025-01-19 19:47:28 -0500

    first try

---
+++ src/app/components/Work.tsx
@@ -0,0 +1,25 @@
+import Image from "react"
+import backgroundImage from "../images/delwatergap.jpg"
+import backgroundImage2 from "../images/pride-dog.jpg"
+
+interface WorkProps {
+    title: string,
+    imageURL: string,
+    type: string
+}
+
+export default function Work({title, imageURL, type}: WorkProps) {
+
+    // const bg_image: string = "bg-[url(" + imageURL + ")]";
+
+    // const background: string = "bg-[url(" + imageURL + ")]";
+    
+    return (
+        <a className={`${type} relative h-64 w-[300px] bg-cover bg-center hover:rounded-2xl duration-500`} style={{backgroundImage: `url(${imageURL})`}}>
+            <div className="title-container hover:bg-white hover:bg-opacity-50 inset-0 absolute duration-500 flex justify-center items-center">
+                <p className="title opacity-100 hidden">{title}</p>
+            </div>
+        </a>
+    )
+    
+}
